bf36ac2091d644ad86303f87c57794f911e167a8
ggalvisgg <gabrielagalvisgg@gmail.com>
2025-05-19 17:36:06 -0500

    update 2.0

--- repositories/book_repository.go
+++ repositories/book_repository.go
@@ -2,7 +2,7 @@ package repositories
 
 import (
     "context"
-    "go.mongodb.org/mongo-driver/bson"
+    //"go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
     "go.mongodb.org/mongo-driver/bson/primitive"
